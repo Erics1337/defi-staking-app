92c544d48c30d0fe8f624c9c2b623e4113951ec0
Eric <erics1337@gmail.com>
2022-04-28 16:06:07 -0600

    transferFrom function

--- src/contracts/Tether.sol
+++ src/contracts/Tether.sol
@@ -6,15 +6,18 @@ contract Tether {
     uint256 public totalSupply = 1000000000000000000; // 1 million tokens
     uint8 public decimals = 18;
 
+    // Events are camelCased by convention
     event Transfer(address indexed from, address indexed to, uint256 value);
 
-    event Approve(
+    // Allow the user an additional security measure to approve transfer event
+    event Approval(
         address indexed owner,
         address indexed spender,
         uint256 value
     );
 
     mapping(address => uint256) public balanceOf;
+    mapping(address => mapping(address => uint256)) public allowance;
 
     constructor() {
         balanceOf[msg.sender] = totalSupply;
@@ -27,6 +30,24 @@ contract Tether {
         balanceOf[msg.sender] -= value;
         // Add the balance to the recipient
         balanceOf[to] += value;
+        // Emit the transfer event
+        // Outside consumer can see the event through web3js
+        emit Transfer(msg.sender, to, value);
+        return true;
+    }
+
+    function transferFrom(
+        address from,
+        address to,
+        uint256 value
+    ) public returns (bool success) {
+        require(value <= balanceOf[from]);
+        require(value <= allowance[from][msg.sender]);
+        // Add the balance for transferFrom
+        balanceOf[to] += value;
+        // Subtract the balance for transferFrom
+        balanceOf[from] -= value;
+        allowance[msg.sender][from] -= value;
         // Emit the transfer event
         emit Transfer(from, to, value);
         return true;
