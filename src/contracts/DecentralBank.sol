878fbbfbb7545f4c83f7897365a53c41aaeec1df
Eric <erics1337@gmail.com>
2022-04-29 13:37:28 -0600

    Staking Logic

--- src/contracts/DecentralBank.sol
+++ src/contracts/DecentralBank.sol
@@ -8,8 +8,32 @@ contract DecentralBank {
     Tether public tether;
     Reward public reward;
 
+    // Key store
+    address[] public stakers;
+
+    mapping(address => uint) public stakingBalance;
+    mapping(address => bool) public hasStaked;
+    mapping(address => bool) public isStaked;
+
     constructor(Reward reward, Tether tether) public {
         reward = reward;
         tether = tether;
+    }
+
+    // Staking function 
+    function depositTokens(uint amount) public {
+        // Transfer Tether tokens to this address for staking
+        tether.transferFrom(msg.sender, address(this), amount);
+
+        // Update staking balance
+        stakingBalance[msg.sender] = stakingBalance[msg.sender] + amount;
+
+        if(!hasStaked) {
+            stakers.push[msg.sender];
+        }
+
+        // Update staking balance
+        isStaking[msg.sender] = true;
+        hasStaked[msg.sender] = true;
     }
 }
