d73ff99fda005f63f86ccae9459eef7e944cf277
Eric <erics1337@gmail.com>
2022-05-02 11:30:42 -0600

    state and setAccount

--- src/components/App.js
+++ src/components/App.js
@@ -3,6 +3,15 @@ import Navbar from './Navbar'
 import Web3 from 'web3'
 
 function App() {
+	const [account, setAccount] = useState('0x0')
+	const [tether, setTether] = useState('0x0')
+	const [reward, setReward] = useState('0x0')
+	const [decentralBank] = useState('0x0')
+	const [tetherBalance] = useState('0x0')
+	const [rewardBalance] = useState('0x0')
+	const [stakingBalance] = useState('0x0')
+	const [loading, setLoading] = useState(true)
+
 	// To use async functions inside useEffect, we need to defne them inside
 	// Or if you need to have async function outside useEffect, wrap it in useCallback and cal it in the useEffect function and add it to dependency array
 	// Simple Rule: If any variable is used inside the hook but is defined outside of it, then it should go in the dependency array. That goes both for simple variables as well as for functions.
@@ -23,14 +32,12 @@ function App() {
 		const loadBlockchainData = async () => {
 			const web3 = window.web3
 			const accounts = await web3.eth.getAccounts()
-			console.log(accounts)
+			setAccount(accounts[0])
 		}
 
 		loadWeb3().catch(console.error)
 		loadBlockchainData().catch(console.error)
 	}, [])
-
-	const [account, setAccount] = useState('0x0')
 
 	return (
 		<div>
